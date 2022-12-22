fe41ab0adc0e418ddea703003f7849b3b81435c4
HarshithGV <93967468+HarshithGV@users.noreply.github.com>
2022-12-22 21:05:25 +0530

    Add files via upload

---
+++ Counter.svelte
@@ -0,0 +1,93 @@
+<script>
+	import { onMount } from "svelte";
+	import { apiData, drinkNames } from './store.js';
+	
+	onMount(async () => {
+	  fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+	  .then(response => response.json())
+	  .then(data => {
+			console.log(data);
+		apiData.set(data);
+	  }).catch(error => {
+		console.log(error);
+		return [];
+	  });
+	});
+	</script>
+	
+	<main>
+		<h1>Whiskey Drinks Menu</h1>
+		<ul>
+		{#each $drinkNames as drinkName}
+			<li>{drinkName}</li>
+		{/each}
+		</ul>
+	</main>
+	
+	
+<style>
+	.counter {
+		display: flex;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+
+	.counter button:hover {
+		background-color: var(--color-bg-1);
+	}
+
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: #444;
+	}
+
+	.counter-viewport {
+		width: 8em;
+		height: 4em;
+		overflow: hidden;
+		text-align: center;
+		position: relative;
+	}
+
+	.counter-viewport strong {
+		position: absolute;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		font-weight: 400;
+		color: var(--color-theme-1);
+		font-size: 4rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.counter-digits {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	.hidden {
+		top: -100%;
+		user-select: none;
+	}
+</style>
